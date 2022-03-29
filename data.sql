7ed667cc8cede1ca5bff7fcf3df8c1bff6939479
Yerimah <danielyerimah@yahoo.com>
2022-03-29 15:57:39 +0100

    Inserted new data, wrote queries for the input

--- data.sql
+++ data.sql
@@ -49,3 +49,152 @@ INSERT INTO animals (
         5, 
         true, 
         11);
+
+-- Insert new data into table 
+INSERT INTO animals (
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, 
+    weight_kg) 
+    VALUES (
+        'Charmander', 
+        '2020-02-08', 
+        0, 
+        false, 
+        -11);
+
+INSERT INTO animals (
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, 
+    weight_kg) 
+    VALUES (
+        'Plantmon', 
+        '2021-11-15', 
+        2, 
+        true, 
+        -5.7);
+
+INSERT INTO animals (
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, 
+    weight_kg) 
+    VALUES (
+        'Squirtle', 
+        '1993-04-02', 
+        3, 
+        false, 
+        -12.13);
+
+INSERT INTO animals (
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, 
+    weight_kg) 
+    VALUES (
+        'Angemon', 
+        '2005-06-12', 
+        1, 
+        true, 
+        -45);
+
+INSERT INTO animals (
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, 
+    weight_kg) 
+    VALUES (
+        'Boarmon', 
+        '2005-06-07', 
+        7, 
+        true, 
+        20.4);
+
+INSERT INTO animals (
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, 
+    weight_kg) 
+    VALUES (
+        'Blossom', 
+        '1998-10-13', 
+        3, 
+        true, 
+        17);
+
+INSERT INTO animals (
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, 
+    weight_kg) 
+    VALUES (
+        'Ditto', 
+        '2022-05-14', 
+        4, 
+        true, 
+        22);
+
+-- setting the species column to unspecified
+BEGIN; -- start transaction
+
+UPDATE animals SET species = 'unspecified';
+
+-- Verify that change was made
+SELECT * FROM animals;
+
+-- Then roll back the change
+ROLLBACK;
+
+-- verify that species columns went back to the state before transaction.
+SELECT * FROM animals;
+
+/* Adding animal species*/
+
+BEGIN; -- start transaction
+
+-- setting the species column to digimon for all animals that have a name ending in mon.
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+-- setting the species column to pokemon for all animals that don't have species already set.
+Update animals SET species = 'pokemon' WHERE species IS NULL;
+
+COMMIT; --Commit the transaction.
+
+-- Verify that change was made and persists after commit.
+SELECT * FROM animals;
+
+/* Deleting all animal*/
+BEGIN;
+
+DELETE FROM animals;
+
+-- verify that the animals have been deleted
+SELECT * FROM animals;
+
+ROLLBACK;
+
+-- very that table content are present
+SELECT * FROM animals;
+
+-- savepoint
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2020-01-01';
+
+SAVEPOINT save_point;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO save_point;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
